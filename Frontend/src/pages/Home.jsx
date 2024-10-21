a989c8fdfc3992251a392df0433deb734f07a22e
sam1132 <sam113273@gmail.com>
2024-10-21 22:33:15 +0530

    Refactor Home page and add service icons

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -4,12 +4,14 @@ import heroImg from "../assets/hero.jpg";
 import { CiForkAndKnife } from "react-icons/ci";
 import { GiClothes } from "react-icons/gi";
 import { ImBooks } from "react-icons/im";
-
+import { FaChalkboardTeacher,FaTshirt } from "react-icons/fa";
+import { MdOutlineSportsCricket } from "react-icons/md";
+import { FaBowlFood } from "react-icons/fa6";
 const Home = () => {
   return (
     <>
       <Navbar />
-      <section>
+      <main>
         <header className="max-w-[95rem] mx-auto h-[356px] bg-[#00b4d8] px-8 md:px-10 pt-7  md:pt-0 ">
           <div className="flex flex-col md:flex-row  ">
             <div className="md:mt-20">
@@ -42,10 +44,28 @@ const Home = () => {
           </div>
         </header>
         <section className="max-w-[95rem] mx-auto px-8 md:px-10 my-5">
-              
+          <h1 className="text-center font-bold text-3xl md:text-5xl text-[#ff3333] my-10">Our Services</h1>
+              <div className="flex justify-center items-center gap-y-10 gap-x-20 md:flex-row flex-col cursor-pointer">
+                    <div className="h-32 w-32 rounded-t-full border relative bg-gray-100">
+                        <span><FaChalkboardTeacher className="absolute right-[3rem] text-[#00b4d8] top-[1.5rem] h-10 w-10"/></span>
+                        <p className="absolute top-[5rem] right-[1.3rem] font-semibold text-xl text-gray-600">Education</p>
+                    </div>
+                    <div className="h-32 w-32 rounded-t-full border relative bg-gray-100">
+                        <span><FaBowlFood className="absolute right-[2.7rem] text-[#00b4d8] top-[1.5rem] h-10 w-10"/></span>
+                        <p className="absolute top-[5rem] right-[2.5rem] font-semibold text-xl text-gray-600">Food</p>
+                    </div>
+                    <div className="h-32 w-32 rounded-t-full border relative bg-gray-100">
+                        <span><MdOutlineSportsCricket className="absolute right-[2.7rem] text-[#00b4d8] top-[1.5rem] h-10 w-10"/></span>
+                        <p className="absolute top-[5rem] right-[2rem] font-semibold text-xl text-gray-600">Sports</p>
+                    </div>
+                    <div className="h-32 w-32 rounded-t-full border relative bg-gray-100">
+                        <span><FaTshirt className="absolute right-[2.7rem] text-[#00b4d8] top-[1.5rem] h-10 w-10"/></span>
+                        <p className="absolute top-[5rem] right-[2rem] font-semibold text-xl text-gray-600">Cloths</p>
+                    </div>
+              </div>
         </section>
         <section>
-          <div className="max-w-[95rem] mx-auto px-8 md:px-10 py-10">
+          <div className="max-w-[95rem] mx-auto px-8 md:px-10 py-10 mt-10">
             <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl text-center  md:text-4xl lg:text-5xl/none text-[#ff3333]">
               Ways to Help
             </h2>
@@ -124,7 +144,7 @@ const Home = () => {
                   role="tab"
                   aria-controls="stats"
                   aria-selected="true"
-                  className="inline-block w-full p-4 rounded-ss-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"
+                  className="inline-block w-full p-4 rounded-ss-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600 font-bold text-3xl text-[#00b4d8]"
                 >
                   Statistics
                 </button>
@@ -141,7 +161,7 @@ const Home = () => {
                 role="tabpanel"
                 aria-labelledby="stats-tab"
               >
-                <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-4 dark:text-white sm:p-8">
+                <dl className="grid max-w-screen-xl grid-cols-2 gap-4 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-4 dark:text-white sm:p-8">
                   <div className="flex flex-col items-center justify-center">
                     <dt className="mb-2 text-2xl md:text-3xl font-extrabold">2M+</dt>
                     <dd className="text-gray-500 dark:text-gray-400">Peoples</dd>
@@ -170,8 +190,28 @@ const Home = () => {
             </div>
           </div>
         </section>
-
-      </section>
+        {/* news letter */}
+        <section className="max-w-[95rem] mx-auto px-8 bg-[#00b4d8] h-72 md:h-96 md:px-10  my-5">
+          <div className=" text-white p-8 rounded-md ">
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl text-center  md:text-4xl lg:text-5xl/none">
+              Subscribe to our Newsletter
+            </h2>
+            <p className="text-center mt-4">
+              Get the latest news and updates on our services and events
+            </p>
+            <div className="flex md:flex-row flex-col gap-3 justify-center mt-4">
+              <input
+                type="email"
+                placeholder="Enter your email address"
+                className="p-2 w-80 rounded-md"
+              />
+              <button className="bg-[#ff3333] block w-36  md:ml-2 cursor-pointer text-white font-semibold p-2 rounded-md">
+                Subscribe
+              </button>
+            </div>
+          </div>
+        </section>
+      </main>
     </>
   );
 };
