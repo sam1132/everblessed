2b2335a759e02cee0890d59f47a68dc1b42a1a8d
sam1132 <sam113273@gmail.com>
2024-10-21 17:43:29 +0530

    Refactor package.json and add react-icons dependency and home page code

---
+++ Frontend/src/components/Navbar.jsx
@@ -0,0 +1,188 @@
+import React, { useState, useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { CgProfile } from "react-icons/cg";
+import { FaBoxArchive, FaHeart, FaBars } from "react-icons/fa6";
+import { FaTimes } from "react-icons/fa";
+
+
+const Navbar = () => {
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+    navigate("/signin");
+  };
+
+  return (
+    <nav className="bg-white shadow-md mx-2">
+      <div className="max-w-[95rem] mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-between h-16">
+          <div className="flex items-center">
+            <div className="flex-shrink-0 flex items-center">
+              <NavLink
+                to="/"
+                className="ml-2 text-xl font-bold flex items-center gap-x-2"
+              >
+                EverBlessed
+              </NavLink>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <div className="hidden md:flex">
+              {isAuthenticated ? (
+                <>
+                  <button
+                    onClick={handleLogout}
+                    className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md text-md md:text-2xl font-medium"
+                  >
+                    Logout
+                  </button>
+                </>
+              ) : (
+                <>
+                  <NavLink
+                    to="/login"
+                    className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md text-md md:text-2xl font-medium"
+                  >
+                    Login
+                  </NavLink>
+                  <NavLink
+                    to="/signin"
+                    className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md text-md md:text-2xl font-medium"
+                  >
+                    SignIn
+                  </NavLink>
+                </>
+              )}
+              <NavLink
+                to="/donate"
+                className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md text-md md:text-2xl font-medium"
+              >
+                Donate
+              </NavLink>
+              <div className="relative">
+                <button
+                  onClick={() => setDropdownOpen(!isDropdownOpen)}
+                  className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md text-md md:text-2xl font-medium"
+                >
+                  Account
+                </button>
+                {isDropdownOpen && (
+                  <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10">
+                    <div
+                      className="py-1"
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="options-menu"
+                    >
+                      <NavLink
+                        to="/profile"
+                        className="flex gap-x-2 items-center px-4 py-2 text-lg text-gray-700 hover:text-black hover:bg-gray-100"
+                        role="menuitem"
+                      >
+                        <span>
+                          <CgProfile />
+                        </span>
+                        Profile
+                      </NavLink>
+                      <NavLink
+                        to="/mydonations"
+                        className="flex items-center gap-x-2 px-4 py-2 text-lg text-gray-700 hover:text-black hover:bg-gray-100"
+                        role="menuitem"
+                      >
+                        <span>
+                          <FaBoxArchive className="h-4 w-4" />
+                        </span>
+                        My Donation
+                      </NavLink>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="md:hidden flex items-center">
+              <button
+                onClick={() => setMobileMenuOpen(!isMobileMenuOpen)}
+                className="text-gray-700 hover:text-blue-500 px-3 py-2 rounded-md text-md md:text-2xl font-medium"
+              >
+                {isMobileMenuOpen ? <FaTimes /> : <FaBars />}
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      {isMobileMenuOpen && (
+        <div className="md:hidden">
+          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+            {isAuthenticated ? (
+              <>
+                <button
+                  onClick={handleLogout}
+                  className="text-gray-700 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Logout
+                </button>
+              </>
+            ) : (
+              <>
+                <NavLink
+                  to="/login"
+                  className="text-gray-700 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Login
+                </NavLink>
+                <NavLink
+                  to="/signin"
+                  className="text-gray-700 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  SignIn
+                </NavLink>
+              </>
+            )}
+            <NavLink
+              to="/donate"
+              className="text-gray-700 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Donate
+            </NavLink>
+            <button
+              onClick={() => setDropdownOpen(!isDropdownOpen)}
+              className="text-gray-700 hover:text-blue-500 block px-3 py-2 rounded-md text-base font-medium"
+            >
+              Account
+            </button>
+            {isDropdownOpen && (
+              <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <NavLink
+                  to="/profile"
+                  className="text-gray-700 hover:text-black block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Profile
+                </NavLink>
+                <NavLink
+                  to="/mydonations"
+                  className="text-gray-700 hover:text-black block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  My donation
+                </NavLink>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,9 +1,179 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
+import heroImg from "../assets/hero.jpg";
+import { CiForkAndKnife } from "react-icons/ci";
+import { GiClothes } from "react-icons/gi";
+import { ImBooks } from "react-icons/im";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <Navbar />
+      <section>
+        <header className="max-w-[95rem] mx-auto h-[356px] bg-[#00b4d8] px-8 md:px-10 pt-7  md:pt-0 ">
+          <div className="flex flex-col md:flex-row  ">
+            <div className="md:mt-20">
+              <div className="space-y-2">
+                <h1 className="text-2xl font-bold tracking-tighter sm:text-3xl md:text-4xl lg:text-5xl/none text-white ">
+                  Bless Others Through Your
+                  <span className="block">Generosity</span>
+                </h1>
+                <p className="max-w-[600px] text-white  md:text-xl">
+                  Your donations of food, clothes, or books can bring hope to
+                  the elderly and children in need.
+                </p>
+              </div>
+              <div className="flex flex-col md:flex-row mt-9">
+                <button className="bg-[#ff5252] hover:bg-[#ff3333] cursor-pointer text-white font-semibold p-2 rounded-md">
+                  Donate Now
+                </button>
+                <button className="bg-white text-[#00b4d8] hover:bg-gray-100 p-2 rounded-md font-semibold md:ml-4 mt-4 md:mt-0">
+                  Learn More
+                </button>
+              </div>
+            </div>
+            <div className="ml-48  p-3">
+              <img
+                src={heroImg}
+                alt="donation"
+                className="w-full h-[330px] object-cover rounded-md md:block hidden"
+              />
+            </div>
+          </div>
+        </header>
+        <section className="max-w-[95rem] mx-auto px-8 md:px-10 my-5">
+              
+        </section>
+        <section>
+          <div className="max-w-[95rem] mx-auto px-8 md:px-10 py-10">
+            <h2 className="text-2xl font-bold tracking-tighter sm:text-3xl text-center  md:text-4xl lg:text-5xl/none text-[#ff3333]">
+              Ways to Help
+            </h2>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+              <div className=" px-4 py-6 rounded-md shadow-md border">
+                <p className="flex items-center justify-center">
+                  <CiForkAndKnife className="w-8 h-8  text-[#00b4d8]" />
+                </p>
+                <h3 className="text-xl font-bold text-center ">Donate Food</h3>
+                <p className="mt-2 text-gray-500 px-4 text-center">
+                  Help fight hunger by donating non-perishable food items.
+                </p>
+                <div className="flex justify-center">
+                  <button className="bg-[#ff3333] mt-4  cursor-pointer text-white font-semibold p-2 rounded-md">
+                    Donate Food
+                  </button>
+                </div>
+              </div>
+              <div className=" px-4 py-6 rounded-md shadow-md border">
+                <p className="flex items-center justify-center">
+                  <ImBooks className="w-8 h-8  text-[#00b4d8]" />
+                </p>
+                <h3 className="text-xl font-bold text-center ">Donate Books</h3>
+                <p className="mt-2 text-gray-500 px-4 text-center">
+                  Spread knowledge and joy by donating books for all ages
+                </p>
+                <div className="flex justify-center">
+                  <button className="bg-[#ff3333] mt-4  cursor-pointer text-white font-semibold p-2 rounded-md">
+                    Donate Books
+                  </button>
+                </div>
+              </div>
+              <div className=" px-4 py-6 rounded-md shadow-md border">
+                <p className="flex items-center justify-center">
+                  <GiClothes className="w-8 h-8  text-[#00b4d8]" />
+                </p>
+                <h3 className="text-xl font-bold text-center ">
+                  Donate Clothes
+                </h3>
+                <p className="mt-2 text-gray-500 px-4 text-center">
+                  Provide warmth and dignity with gently used or new clothing
+                </p>
+                <div className="flex justify-center">
+                  <button className="bg-[#ff3333] mt-4  cursor-pointer text-white font-semibold p-2 rounded-md">
+                    Donate Clothes
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="max-w-[95rem] mx-auto px-8 md:px-10 my-5">
+          <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="sm:hidden">
+              <label htmlFor="tabs" className="sr-only ">
+                Select tab
+              </label>
+              <select
+                id="tabs"
+                className="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 text-xl font-semibold text-center rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option value="#stats">Statistics</option>
+              </select>
+            </div>
 
-export default Home
+            <ul
+              className="hidden text-sm font-medium text-center text-gray-500 divide-x divide-gray-200 rounded-lg sm:flex dark:divide-gray-600 dark:text-gray-400 rtl:divide-x-reverse"
+              id="fullWidthTab"
+              role="tablist"
+            >
+              <li className="w-full">
+                <button
+                  id="stats-tab"
+                  data-tabs-target="#stats"
+                  type="button"
+                  role="tab"
+                  aria-controls="stats"
+                  aria-selected="true"
+                  className="inline-block w-full p-4 rounded-ss-lg bg-gray-50 hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"
+                >
+                  Statistics
+                </button>
+              </li>
+            </ul>
+
+            <div
+              id="fullWidthTabContent"
+              className="border-t border-gray-200 dark:border-gray-600"
+            >
+              <div
+                className="p-4 bg-white rounded-lg md:p-8 dark:bg-gray-800"
+                id="stats"
+                role="tabpanel"
+                aria-labelledby="stats-tab"
+              >
+                <dl className="grid max-w-screen-xl grid-cols-2 gap-8 p-4 mx-auto text-gray-900 sm:grid-cols-3 xl:grid-cols-4 dark:text-white sm:p-8">
+                  <div className="flex flex-col items-center justify-center">
+                    <dt className="mb-2 text-2xl md:text-3xl font-extrabold">2M+</dt>
+                    <dd className="text-gray-500 dark:text-gray-400">Peoples</dd>
+                  </div>
+                  <div className="flex flex-col items-center justify-center">
+                    <dt className="mb-2 text-2xl md:text-3xl font-extrabold">4M+</dt>
+                    <dd className="text-gray-500 dark:text-gray-400">
+                      Donations
+                    </dd>
+                  </div>
+                  <div className="flex flex-col items-center justify-center">
+                    <dt className="mb-2 text-2xl md:text-3xl font-extrabold">500+</dt>
+                    <dd className="text-gray-500 dark:text-gray-400">
+                      Active User
+                    </dd>
+                  </div>
+                 
+                  <div className="flex flex-col items-center justify-center">
+                    <dt className="mb-2 text-3xl font-extrabold">90+</dt>
+                    <dd className="text-gray-500 dark:text-gray-400">
+                      NGO's
+                    </dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+          </div>
+        </section>
+
+      </section>
+    </>
+  );
+};
+
+export default Home;
